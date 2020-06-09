f1e7eedf9bcfb8bc355152ae2a025cadfb3f2c97
Kevin Hill <kahill2014@gmail.com>
2020-06-08 21:57:06 -0500

    add event listeners on boxes to display a message when clicked

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ for (var i=0; i < boxes.length; i++){
 		}
 	}, false);
 }
+var newEl = document.createElement('p');
+/* Box 1 clicked - display message */
+document.getElementById('box-1').addEventListener('click', function(){
+	newEl.innerText = 'Oooh - so close, but no cigar';
+	document.getElementById('box-results').append(newEl);
+}, false);
+
+/* Box 2 clicked - display message */
+document.getElementById('box-2').addEventListener('click', function(){
+	newEl.innerText = 'DING DING DING - We have a winner';
+	document.getElementById('box-results').append(newEl);
+}, false);
+
+/* Box 3 clicked - display message */
+document.getElementById('box-3').addEventListener('click', function(){
+	newEl.innerText = 'Oops, butter luck next time';
+	document.getElementById('box-results').append(newEl);
+}, false);
